e1d319ba817db1a10289d6afc1f48f9d8b4963bf
VStepanov <v.stepanov@otpbank.ru>
2020-01-16 15:59:15 +0300

    Layout of input component

--- src/atomic/Input/index.js
+++ src/atomic/Input/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+import styles from './index.module.css'
 
 const Input = props => {
-  return <input />
+  return (
+    <label className={styles.inputContainer}>
+      <span className={styles.inputLabel}>Label</span>
+      <input className={styles.input} />
+    </label>
+  )
 }
 
 export default Input
---
+++ src/atomic/Input/readme.md
@@ -0,0 +1,5 @@
+#### Input component
+
+```js
+<Input />
+```
