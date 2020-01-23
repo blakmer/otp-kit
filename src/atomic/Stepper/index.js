b5895eba4393d91d7a61766832045610795c8910
VStepanov <v.stepanov@otpbank.ru>
2020-01-23 11:09:26 +0300

    Stepper component

--- src/atomic/Stepper/index.js
+++ src/atomic/Stepper/index.js
@@ -5,25 +5,59 @@ import classnames from 'classnames'
 import styles from './index.module.css'
 
 const Stepper = props => {
+  const { steps, currentStep, onChange, closeNextStepPerClick } = props
+
+  const handleChange = step => {
+    let accept = true
+    if (closeNextStepPerClick && currentStep <= step) {
+      accept = false
+    }
+    if (onChange && accept) onChange({ step })
+  }
+
   return (
     <div className={styles.stepContainer}>
-      <div className={classnames(styles.step, styles.successStep)}>
-        <div className={classnames(styles.stepCircle)}>1</div>
-        <div className={classnames(styles.stepTitle)}>Title</div>
-        <div className={classnames(styles.stepDescription)}>Description</div>
-      </div>
-      <div className={classnames(styles.step, styles.currentStep)}>
-        <div className={classnames(styles.stepCircle)}>2</div>
-        <div className={classnames(styles.stepTitle)}>Title</div>
-        <div className={classnames(styles.stepDescription)}>Description</div>
-      </div>
-      <div className={classnames(styles.step)}>
-        <div className={classnames(styles.stepCircle)}>3</div>
-        <div className={classnames(styles.stepTitle)}>Title</div>
-        <div className={classnames(styles.stepDescription)}>Description</div>
-      </div>
+      {steps.map((i, k) => (
+        <div
+          className={classnames(
+            styles.step,
+            currentStep > k && styles.successStep,
+            currentStep === k && styles.currentStep
+          )}
+          key={k}>
+          <div
+            onClick={() => handleChange(k)}
+            className={classnames(styles.stepCircle)}>
+            {k + 1}
+          </div>
+          <div className={classnames(styles.stepTitle)}>{i.title}</div>
+          <div
+            onClick={() => handleChange(k)}
+            className={classnames(styles.stepDescription)}>
+            {i.description}
+          </div>
+        </div>
+      ))}
     </div>
   )
+}
+
+Stepper.propTypes = {
+  steps: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+      description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    })
+  ),
+  currentStep: PropTypes.number,
+  onChange: PropTypes.func,
+  closeNextStepPerClick: PropTypes.bool,
+}
+
+Stepper.defaultProps = {
+  steps: [],
+  currentStep: 0,
+  closeNextStepPerClick: true,
 }
 
 export default Stepper
