e5e688243c44b7c5a69ad908b370bd2e52e63879
StepanovVG <stepanovvg@n00110424.local>
2020-03-03 17:29:42 +0300

    Add simple file loader

--- src/Uploader/index.js
+++ src/Uploader/index.js
@@ -1,8 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Icon from '../Icon'
+import Typography from '../Typography'
+
+import styles from './index.module.css'
 
 const Uploader = props => {
-  return <div></div>
+  const handleChange = e => {
+    console.log(e.target.files)
+  }
+
+  const handleDrop = e => {
+    e.preventDefault()
+    console.log(e.dataTransfer.files)
+  }
+
+  return (
+    <label
+      className={styles.uploader}
+      onDrop={e => handleDrop(e)}
+      onDragOver={e => e.preventDefault()}>
+      <Icon.ClipArt type="image-upload" />
+      <Typography.Text size="small" align="center">
+        Переместите сюда изображения для загрузки
+      </Typography.Text>
+      <input type="file" onChange={e => handleChange(e)} multiple />
+    </label>
+  )
 }
 
 Uploader.propTypes = {}
