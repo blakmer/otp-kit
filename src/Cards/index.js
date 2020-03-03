683f997357b4b9a32f2c1f9e4930cfddce8d05ad
StepanovVG <stepanovvg@n00110424.local>
2020-03-03 09:27:28 +0300

    fix small bug in card component

--- src/Cards/index.js
+++ src/Cards/index.js
@@ -1,26 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import styles from './index.module.css'
 import classnames from 'classnames'
 import Typography from '../Typography'
 
 const Cards = props => {
-  const { 
-    img,
-    body,
-    header,
-    footer,
-    shadow,
-    className,
-    style
-  } = props 
- 
+  const { img, body, header, footer, shadow, className, style } = props
+
   return (
-    <div style={style} className={classnames(styles.card, shadow ? styles.cardShadow : '', className)}>
-      {typeof img !=="undefined" && <div className={styles.img} style={{backgroundImage: `url(${img})`}} />}
+    <div
+      style={style}
+      className={classnames(
+        styles.card,
+        shadow ? styles.cardShadow : '',
+        className
+      )}>
+      {typeof img !== 'undefined' && (
+        <div
+          className={styles.img}
+          style={{ backgroundImage: `url(${img})` }}
+        />
+      )}
       <div className={styles.body}>
-        <div className={styles.header}><Typography.Heading level={2}>{header}</Typography.Heading></div>
-        <div >{body}</div>
+        <div className={styles.header}>
+          <Typography.Heading level={2}>{header}</Typography.Heading>
+        </div>
+        <div>{body}</div>
         <div className={styles.footer}>{footer}</div>
       </div>
     </div>
@@ -31,10 +36,10 @@ Cards.propTypes = {
   img: PropTypes.string,
   className: PropTypes.string,
   style: PropTypes.object,
-  shadow: Proptypes.bool,
+  shadow: PropTypes.bool,
   header: PropTypes.node,
   body: PropTypes.node,
-  footer: PropTypes.node
+  footer: PropTypes.node,
 }
 
 export default Cards
