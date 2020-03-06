e19036c83440519a72a6deb6f0623dcef3a7bd67
Ezhov Alexander <A.Ezhov@otpbank.ru>
2020-03-06 11:48:58 +0300

    Added opportunity to change colors in tooltip component using prop fill

---
+++ src/Tooltip/fills.js
@@ -0,0 +1,37 @@
+import { COLORS } from '../docs'
+
+const {
+  TEXT_PRIMARY,
+  TEXT_SECONDARY,
+  TEXT_DISABLED,
+  TEXT_INVERSE,
+  PRIMARY,
+  PRIMARY_HOVER,
+  HIGHLIGHTED,
+  ERROR,
+  ERROR_HOVER,
+  BLUE,
+  BLUE_HOVER,
+  BG_GREY_1,
+  BG_GREY_2,
+  BG_GREY_3,
+  BG_GREY_4,
+} = COLORS
+
+export const FILLS = {
+  primary: TEXT_PRIMARY,
+  secondary: TEXT_SECONDARY,
+  disabled: TEXT_DISABLED,
+  inverse: TEXT_INVERSE,
+  green: PRIMARY,
+  greenHover: PRIMARY_HOVER,
+  highlighted: HIGHLIGHTED,
+  danger: ERROR,
+  dangerHover: ERROR_HOVER,
+  blue: BLUE,
+  blueHover: BLUE_HOVER,
+  grey1: BG_GREY_1,
+  grey2: BG_GREY_2,
+  grey3: BG_GREY_3,
+  grey4: BG_GREY_4,
+}
