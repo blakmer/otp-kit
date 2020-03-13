c82a96544817dfce4ec4e6767bce53a880bf251c
StepanovVG <stepanovvg@n00110424.local>
2020-03-13 13:12:15 +0300

    Empty layout for Round progress component

---
+++ src/RoundProgress/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const RoundProgress = props => {
+  return <div>1</div>
+}
+
+RoundProgress.propTypes = {}
+
+export default RoundProgress
---
+++ src/RoundProgress/readme.md
@@ -0,0 +1,5 @@
+RoundProgress
+
+```js
+<RoundProgress />
+```
