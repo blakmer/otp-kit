8e5e261a9d98edcaac5ec821c4a21449aee97239
StepanovVG <stepanovvg@n00110424.local>
2020-05-28 16:30:44 +0300

    Edit kebab

--- src/Kebab/index.js
+++ src/Kebab/index.js
@@ -1,85 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classnames from 'classnames'
-import { useCombobox } from 'downshift'
+import Dropdown from '../Dropdown'
 import RoundButton from '../RoundButton'
 import styles from './index.module.css'
 
 const Kebab = props => {
-  const { items, onChange, emptyText, block, listDirection, disabled } = props
-
-  const {
-    isOpen,
-    getToggleButtonProps,
-    getMenuProps,
-    getComboboxProps,
-    highlightedIndex,
-    getItemProps,
-  } = useCombobox({
-    items: items,
-    itemToString: i => i.title,
-    onSelectedItemChange: ({ selectedItem }) => {
-      onChange(selectedItem)
-    },
-  })
+  const { items, emptyText, listDirection, disabled } = props
 
   return (
-    <div className={classnames(styles.container, block && styles.block)}>
-      <label
-        {...getComboboxProps()}
-        {...(disabled ? {} : getToggleButtonProps())}
-        className={classnames(styles.labelContainer, block && styles.block)}>
-        <RoundButton
-          className={styles.kebab}
-          icon={!isOpen ? 'menu-kebab' : 'close'}
-          fill="bg-input"
-          iconFill="primary"
-          disabled={disabled}
-        />
-      </label>
-      {isOpen && (
-        <ul
-          {...getMenuProps()}
-          className={classnames(styles.menu, styles[listDirection])}>
-          {items.map((item, index) => (
-            <li
-              className={classnames(
-                highlightedIndex === index && styles.selected
-              )}
-              key={`${index}`}
-              {...getItemProps({ item, index })}>
-              {item.jsx || item.title}
-            </li>
-          ))}
-          {!items.length && <li className={styles.emptyList}>{emptyText}</li>}
-        </ul>
-      )}
-    </div>
+    <Dropdown
+      emptyText={emptyText}
+      listDirection={listDirection}
+      showArrow={false}
+      className={styles.menuWithoutPadding}
+      items={items}>
+      <RoundButton
+        className={styles.kebab}
+        icon={'menu-kebab'}
+        fill="bg-input"
+        iconFill="primary"
+        disabled={disabled}
+      />
+    </Dropdown>
   )
 }
 
 Kebab.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-      jsx: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number,
-        PropTypes.node,
-      ]),
-      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    })
-  ),
-  onChange: PropTypes.func,
+  items: PropTypes.arrayOf(PropTypes.node),
   emptyText: PropTypes.string,
-  listDirection: PropTypes.oneOf(['bottom', 'top']),
+  listDirection: PropTypes.oneOf(['bottomCenter', 'topCenter']),
 }
 
 Kebab.defaultProps = {
   items: [],
   emptyText: '',
-  onChange: value => {},
-  listDirection: 'top',
+  listDirection: 'topCenter',
 }
 
 export default Kebab
--- src/Kebab/readme.md
+++ src/Kebab/readme.md
@@ -1,15 +1,25 @@
 Kebab Menu
 
 ```js
-
 import Icon from '../Icon'
 import classes from '../classes.module.css'
-
-;<div className={classes['bg-grey-2-bg']} style={{padding: '8rem 0 0 1rem'}}>
-  <Kebab 
+;<div className={classes['bg-grey-2-bg']} style={{ padding: '8rem 0 0 1rem' }}>
+  <Kebab
     items={[
-      { title: 'first', jsx:<Icon.Medium fill="primary" type="file-double" onClick={()=>{console.log('click')}} />, value: 1 },
-      { title: 'second', jsx:<Icon.Medium fill="primary" type="download" onClick={()=>{console.log('click')}}/>, value: 2 },
+      <Icon.Medium
+        fill="primary"
+        type="file-double"
+        onClick={() => {
+          console.log('click 1')
+        }}
+      />,
+      <Icon.Medium
+        fill="primary"
+        type="download"
+        onClick={() => {
+          console.log('click 2')
+        }}
+      />,
     ]}
   />
 </div>
