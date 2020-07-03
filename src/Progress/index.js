ffeaec1cff9aee30bb025951ca2df43fc2752f5e
Ezhov Alexander <A.Ezhov@otpbank.ru>
2020-07-03 10:32:00 +0300

    added showPercent and block to progress

--- src/Progress/index.js
+++ src/Progress/index.js
@@ -4,19 +4,28 @@ import styles from './index.module.css'
 import classnames from 'classnames'
 
 const Progress = props => {
-  const { className, style, value, max } = props
+  const { className, style, value, max, showPercent, block } = props
   const percent = value > 0 && max > 0 ? Math.round((value * 100) / max) : 0
 
   return (
-    <div className={classnames(styles.progress, className)} style={style}>
-      <progress value={value} max={max} id="progress" />
-      <label
-        htmlFor="progress"
-        className={classnames({
-          [styles.complete]: percent === 100,
-        })}>
-        {percent}%
-      </label>
+    <div
+      className={classnames(styles.progress, block && styles.block, className)}
+      style={style}>
+      <progress
+        className={block && styles.block}
+        value={value}
+        max={max}
+        id="progress"
+      />
+      {showPercent && (
+        <label
+          htmlFor="progress"
+          className={classnames({
+            [styles.complete]: percent === 100,
+          })}>
+          {percent}%
+        </label>
+      )}
     </div>
   )
 }
@@ -26,6 +35,12 @@ Progress.propTypes = {
   style: PropTypes.object,
   value: PropTypes.number,
   max: PropTypes.number,
+  block: PropTypes.bool,
+  showPercent: PropTypes.bool,
+}
+
+Progress.defaultProps = {
+  showPercent: true,
 }
 
 export default Progress
