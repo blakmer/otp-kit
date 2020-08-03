9d8d940f079c6bb7481a4f79c6d16f2e1291608f
StepanovVG <stepanovvg@n00110424.local>
2020-08-03 15:13:18 +0300

    Add grid breakpoints system

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -12,12 +12,12 @@ import {
 } from 'react-grid-system'
 
 setConfiguration({
-  // sm, md, lg, xl
-  breakpoints: [768, 1024, 1280, 1536],
-  containerWidths: [744, 1000, 1256, 1512],
+  // sm, md,  lg,  xl,   xxl
+  breakpoints: [576, 768, 992, 1024, 1440],
+  containerWidths: [540, 740, 960, 1024, 1440],
   gutterWidth: 24,
-  gridColumns: 12,
-  defaultScreenClass: 'md',
+  gridColumns: 24,
+  defaultScreenClass: 'xxl',
 })
 
 const Col = props => (
